8caf30367736bee6b58f92da5b796a9d427e0cbd
bartkoo98 <bartosztomczuk12@gmail.com>
2023-04-27 02:32:44 +0200

    Database initialization for dev profile

---
+++ src/main/resources/database/testdata/01_categories.sql
@@ -0,0 +1,3 @@
+insert into category (name) values ('Dietetyka');
+insert into category (name) values ('Informatyka');
+insert into category (name) values ('Polityka');
---
+++ src/main/resources/database/testdata/02_users.sql
@@ -0,0 +1,3 @@
+insert into userAccount(username, email, password) values ('bartkoo', 'bartko@gmail.com', 'bartko12');
+insert into userAccount(username, email, password) values ('michal', 'michal12@o2.com', 'michal98');
+insert into userAccount(username, email, password) values ('halina', 'halina111@gmail.com', 'halina');
